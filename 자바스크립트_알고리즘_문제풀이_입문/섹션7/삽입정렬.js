fcb2729c244ee05df1d9e73f20a1421eeb276625
dayeon <swdy1233@gmail.com>
2023-12-31 01:13:22 +0900

    23/12/31_오전1:13

---
+++ 자바스크립트_알고리즘_문제풀이_입문/섹션7/삽입정렬.js
@@ -0,0 +1,21 @@
+function solution(arr){
+    let answer=arr;
+    for(let i = 1; i<arr.length ; i++){
+        let tmp = arr[i];
+        for(let j = i-1; j>=0 ; j--){
+            if(arr[j]>tmp){
+                arr[j+1] = arr[j];
+            }else{
+                arr[j+1] = tmp;
+                break;
+            }
+            if(j === 0 ){
+                arr[j] = tmp;
+            }
+        }
+    }
+    return answer;
+}
+
+let arr=[11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
